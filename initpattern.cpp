b6562067ff7bbd96d4f0015ae65e9715d5ad6e66
thesourcerer8 <pg@futureware.at>
2023-07-27 20:01:55 +0200

    Update initpattern.cpp

--- initpattern.cpp
+++ initpattern.cpp
@@ -21,8 +21,8 @@ int main(int argc, char* argv[])
   }
   if(argc>2)
   {
-    targetsize=atoi(argv[2])<<11;
-    printf("Setting the target size to %lld sectors (%lld GB).\n",targetsize,targetsize>>30);
+    targetsize=atol(argv[2])<<11;
+    printf("Setting the target size to %ld MB / %lld sectors / %lld GB.\n",targetsize>>11,targetsize,targetsize>>30);
   }
 
   long long border0=512*1024*2; // 512MB pattern
