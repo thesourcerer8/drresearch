1ab073fa448f3b13421a008b68994844ea90d3fb
Philipp Gühring <pg@futureware.at>
2023-07-29 20:13:55 +0200

    Aumatically handling different patterns

--- biterrorstat.pl
+++ biterrorstat.pl
@@ -8,15 +8,41 @@ open IN,"<$fn";
 binmode IN;
 my $sector="";
 my $ende=0;
+my $npage=0;
+my $mode=0;
 while(!$ende)
 {
   my $ret=read IN,$sector,$pagesize;
   last if(!defined($ret) || !$ret);
-  foreach(0 .. 1023)
+  if(substr($sector,0,1) eq "\x77" || substr($sector,10,1) eq "\x77")
   {
-    $stat{substr($sector,$_,1) eq "\x77" ? 1 : 0}++;
+    print "77 Pattern detected!\n";
+    $mode=7;
+    foreach(0 .. 1023)
+    {
+      $stat{substr($sector,$_,1) eq "\x77" ? 1 : 0}++;
+    }
   }
+  elsif($substr($sector,0,6) eq "|Block")
+  {
+    $mode=1;
+  }
+  if(substr($sector,0,1) eq "\x00" || substr($sector,10,1) eq "\x00")
+  {
+    print "00 Pattern detected!\n";
+    $mode=7;
+    foreach(0 .. 1023)
+    {
+      $stat{substr($sector,$_,1) eq "\x77" ? 1 : 0}++;
+    }
+  }
+  else
+  {
+    print "Unknown pattern!\n";	  
+  }
+  $npage++;
 }
 close IN;
 
-print "Statistic for $fn:\nGood: $stat{1}\nBad: $stat{0}\n";
+my $quote=$stat{1}?($stat{1}+$stat{0})/$stat{1}:"perfect";
+print "Statistic for $fn:\nGood: $stat{1}\nBad: $stat{0}\nResult: $quote (higher is better)\n";
