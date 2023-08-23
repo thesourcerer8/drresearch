89a66974b2d30e8e2fefeb9ac952c761abffbf70
Philipp Gühring <pg@futureware.at>
2023-08-23 16:41:50 +0200

    Added pagesize into filenames

--- ecc.bat
+++ ecc.bat
@@ -1,7 +1,7 @@
 echo Create a 3GB Pattern:
 perl initpattern.pl pattern.dump 3000
 echo Simulate the writing the pattern to a NAND controller and dumping the NAND Flash
-perl controllersim.pl pattern.dump simulated.dump chooseyour.case
+perl controllersim.pl pattern.dump "simulated(4000p).dump" chooseyour.case
 echo Extract the necessary parts from the dump for reconstruction
-perl dumpextractrelevant.pl simulated.dump upload.dump 4000
+perl dumpextractrelevant.pl "simulated(4000p).dump" "upload(4000p).dump" 4000
 echo Now please upload the upload.dump to https://futureware.at/cgi-bin/eccupload
