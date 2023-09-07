c901cd802b050ee1aef51e99ea67746ab118380f
Philipp Gühring <pg@futureware.at>
2023-09-07 13:23:14 +0200

    Updated the commandline parameters

--- ecc.bat
+++ ecc.bat
@@ -1,7 +1,7 @@
 echo Create a 3GB Pattern:
-perl initpattern.pl pattern.dump 3000
+perl initpattern.pl pattern.dump 3000 1024
 echo Simulate the writing the pattern to a NAND controller and dumping the NAND Flash
 perl controllersim.pl pattern.dump "simulated(4000p).dump" chooseyour.case
 echo Extract the necessary parts from the dump for reconstruction
 perl dumpextractrelevant.pl "simulated(4000p).dump" "upload(4000p).dump" 4000
-echo Now please upload the upload.dump to https://futureware.at/cgi-bin/eccupload
+echo "Now please provide the pattern.dump.xml and the upload(4000p).dump"
