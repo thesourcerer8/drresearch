2b1212b3316040c34dcad22123f434eea712ba59
Philipp Gühring <pg@futureware.at>
2024-03-28 00:47:05 +0100

    Adding pagesperblock parameter

--- xorviewer.pl
+++ xorviewer.pl
@@ -81,26 +81,26 @@ if($dump !~ m/\.(dump|dmp|raw|bin|img|decoded|g|m|xor)$/)
 
 my $xor=readfile("$dump.xor");
 
-my $pagesperblock=($pagesize && length($xor)) ? length($xor)/int($pagesize) : 1;
+my $pagesperblock=int($in{'pagesperblock'} || (($pagesize && length($xor)) ? length($xor)/int($pagesize) : 1));
 
 my $dumpsize=-s $dump;
 
 if(open($IN,"<$dump"))
 {
   binmode $IN;
-  my $base1="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=";
-  my $base1a="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&xormode=$xormode&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=";
+  my $base1="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&pagesperblock=$pagesperblock&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=";
+  my $base1a="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&pagesperblock=$pagesperblock&xormode=$xormode&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=";
   my $left=$base1.($start-($pagefrag>>1));
   my $right=$base1.($start+($pagefrag>>1));
-  my $base2="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&start=".int($start)."&pagestart=";
+  my $base2="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&pagesperblock=$pagesperblock&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&start=".int($start)."&pagestart=";
   my $up=$base2.($pagestart-1);
   my $down=$base2.($pagestart+1);
   my $pageup=$base2.($pagestart-$pagesperblock);
   my $pagedown=$base2.($pagestart+$pagesperblock);
-  my $base3="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&xormode=\"+mydivtogle+\"&start=".int($start)."&pagestart=".int($pagestart)."&xoroffset=";
+  my $base3="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&pagesperblock=$pagesperblock&xormode=\"+mydivtogle+\"&start=".int($start)."&pagestart=".int($pagestart)."&xoroffset=";
   my $minus=$base3.($xoroffset-1);
   my $plus=$base3.($xoroffset+1);
-  my $toggle="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=".int($start);
+  my $toggle="?dump=".sanitizeHTML($dump)."&pagesize=".sanitizeHTML($pagesize)."&pagesperblock=$pagesperblock&xormode=\"+mydivtoggle+\"&xoroffset=".int($xoroffset)."&pagestart=".int($pagestart)."&start=".int($start);
   print <<EOF
 <html>
 <script>
